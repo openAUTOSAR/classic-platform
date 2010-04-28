f724bd553345bcdddd22a19ffcb6f458c4df8cef
mahi <devnull@localhost>
2010-04-28 21:19:31 +0200

    Added some STM32 boards

---
+++ boards/stm32_mcbstm32/build_config.mk
@@ -0,0 +1,43 @@
+
+# ARCH defines
+ARCH=arm_cm3
+ARCH_FAM=arm
+ARCH_MCU=arm_cm3
+
+#
+# CFG (y/n) macros
+# 
+
+CFG=ARM ARM_CM3
+# Add our board  
+CFG+=BRD_STM32_MCBSTM32 
+
+# 
+# ST have devided devices into ( See chapter 6 in Ref manual )
+# LD - Low Density.    STM32F101xx,F102xx,F103xx). Flash 16->32Kbytes
+# MD - Medium Density. Same as above.              Flash 64->128Kbytes
+# HD - High Denstiry.  STM32F101xx,F103xx.         Flash 256->512Kbytes
+# CL - Connectivity Line. STM32F105xx,F107xx     
+#  
+# [ STM32_MD | CFG_STM32_LD | CFG_STM32_HD | CFG_STM32_CL ]  
+CFG+=STM32_MD
+
+# What buildable modules does this board have, 
+# default or private
+MOD_AVAIL=KERNEL MCU PWM ADC SIMPLE_PRINTF ARM_ITM_TERM RAMLOG DEM IOHWAB
+
+#
+# Modules needed by us
+#
+MOD_USE=KERNEL MCU
+
+#
+# Extra defines 
+#
+
+# Use little heap
+def-y += HEAPSIZE=4000
+# Select the right device in ST header files.
+# [ STM32F10X_LD | STM32F10X_MD |  STM32F10X_HD |STM32F10X_CL ]
+def-y += STM32F10X_MD
+
---
+++ boards/stm32_stm3210c/readme.txt
@@ -0,0 +1,34 @@
+
+
+The STMicroelectronics STM32F107VCT is an ARM 32-bit Cortex-M3 Microcontroller.
+Order info is STM3210C-EVAL.
+	
+Datasheets:
+  STM3210C-EVAL  
+    http://www.st.com/stonline/products/literature/um/15082.pdf  
+ 
+  STM32F107VCT
+    http://www.st.com/stonline/products/literature/ds/15274.pdf
+ 
+Info:	
+  STM32F107VC
+  72MHz
+  256kB Flash 
+  64kB SRAM
+  5 × USARTs,
+  4 × 16-bit timers,
+  2 × basic timers,
+  3 × SPIs,
+  2 × I2S,
+  1 × I2C,
+  USB OTG FS,
+  2 × CANs,
+  1 × PWM timer,
+  2 × ADCs,
+  2 × DACs,
+  Ethernet
+  
+Memory Map:
+ 0x0800_0000 ->           Flash
+ 0x2000_0000 ->           SRAM
+ 0x4000_0000 ->           Internal registers
