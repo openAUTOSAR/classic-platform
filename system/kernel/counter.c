76d841db892a5bb03e15f0a370f0533bbbaf3952
tojo <tobias.johansson@arccore.com>
2010-06-15 14:27:38 +0200

    Fix for problem with multiple Os counters

--- system/kernel/counter.c
+++ system/kernel/counter.c
@@ -202,30 +202,23 @@ TickType GetOsTick( void ) {
  * Initialize alarms and schedule-tables for the counters
  */
 void Os_CounterInit( void ) {
-	OsCounterType *counter;
-	OsAlarmType *alarm_obj;
-	OsSchTblType *sched_obj;
-	/* Create a list from the counter to the alarms */
-	for(int i=0; i < Os_CfgGetCounterCnt() ; i++) {
-		counter = Os_CfgGetCounter(i);
-		// Alarms
-		SLIST_INIT(&counter->alarm_head);
-		for(int j=0; j < Os_CfgGetAlarmCnt(); j++ ) {
-			alarm_obj = Os_CfgGetAlarmObj(j);
-			// Add the alarms
-			SLIST_INSERT_HEAD(&counter->alarm_head,alarm_obj, alarm_list);
-		}
-		// Schedule tables
-		SLIST_INIT(&counter->sched_head);
-		for(int j=0; j < Os_CfgGetSchedCnt(); j++ ) {
-			sched_obj = Os_CfgGetSched(j);
-			// Add the alarms
-			SLIST_INSERT_HEAD(&counter->sched_head,
-								sched_obj,
-								sched_list);
-		}
+	OsCounterType *cPtr;
+	OsAlarmType *aPtr;
+	OsSchTblType *sPtr;
 
+	/* Add the alarms to counters */
+	for(int i=0; i < Os_CfgGetAlarmCnt(); i++ ) {
+		aPtr = Os_CfgGetAlarmObj(i);
+		cPtr = aPtr->counter;
+		SLIST_INSERT_HEAD(&cPtr->alarm_head,aPtr, alarm_list);
+	}
 
+	/* Add the schedule tables to counters */
+	for(int i=0; i < Os_CfgGetSchedCnt(); i++ ) {
+
+		sPtr = Os_CfgGetSched(i);
+		cPtr = sPtr->counter;
+		SLIST_INSERT_HEAD(&cPtr->sched_head, sPtr, sched_list);
 	}
 }
 
