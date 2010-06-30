7af11c8832c5a26aa06c253f51ed1ab8e88c7472
tojo <tobias.johansson@arccore.com>
2010-06-30 14:55:14 +0200

    Makesystem backwards compatability with 2.3.0 projects

--- scripts/rules.mk
+++ scripts/rules.mk
@@ -67,6 +67,28 @@ include $(ROOTDIR)/scripts/cc_$(COMPILER).mk
 # Get object files
 include ../makefile
 
+
+##### For backwards compatability with older project makefiles
+# Remove dependency on libkernel
+deprecated-libs += $(ROOTDIR)/libs/libkernel_$(ARCH_MCU).a
+deprecated-libs-included = $(filter $(deprecated-libs),$(libitem-y))
+ifneq ($(deprecated-libs-included),)
+$(info >>>> Ignoring deprecated lib dependencies: $(deprecated-libs-included)')
+libitem-y := $(filter-out $(deprecated-libs),$(libitem-y))
+endif
+
+# Automatic preprocessing of std linkscripts
+old-ldcmdfile = $(ROOTDIR)/$(ARCH_PATH-y)/scripts/linkscript_gcc.ldf
+new-ldcmdfile = linkscript_gcc.ldp
+old-ldcmdfile-used = $(filter $(old-ldcmdfile),$(ldcmdfile-y))
+ifneq ($(old-ldcmdfile-used),)
+$(info >>>> Changing linkscript to preprocessed version: $(old-ldcmdfile) -> $(new-ldcmdfile)')
+ldcmdfile-y := $(subst $(old-ldcmdfile),$(new-ldcmdfile),$(ldcmdfile-y))
+vpath %.ldf $(ROOTDIR)/$(ARCH_PATH-y)/scripts
+endif
+
+#####
+
 inc-y += $(ROOTDIR)/include
 #inc-$(CFG_PPC) += $(ROOTDIR)/include/ppc
 #inc-$(CFG_ARM) += $(ROOTDIR)/include/arm
