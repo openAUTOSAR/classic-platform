6d0ae48ec8cb4769ec8bad102bad885f9b4e9f46
Niclas <devnull@localhost>
2010-08-23 15:33:59 +0200

    removed rte_start rte_stop because they should be defined in rte_main-h

--- include/Rte.h
+++ include/Rte.h
@@ -129,9 +129,7 @@ typedef uint8 Std_ReturnType;
 //
 
 
-// RTE Lifecycle API reference.( see chap 5.8 )
-Std_ReturnType Rte_Start( void );
-Std_ReturnType Rte_Stop( void );
+
 
 #endif /*RTE_H_*/
 
