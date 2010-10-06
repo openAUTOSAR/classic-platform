3d5f15a53cf46d96ef397400a884a983ac3173aa
maek <devnull@localhost>
2010-10-06 16:39:31 +0200

    Added linux board information and dummy gcc.mk

--- boards/linux/build_config.mk
+++ boards/linux/build_config.mk
@@ -1,5 +1,7 @@
 
 # ARCH defines
+ARCH=linux
+ARCH_FAM=generic
 #ARCH=mpc55xx
 #ARCH_FAM=ppc
 #ARCH_MCU=mpc5516
@@ -9,7 +11,7 @@ CFG= BRD_LINUX
 
 # What buildable modules does this board have, 
 # default or private
-MOD_AVAIL+=COM
+MOD_AVAIL+=COM PDUR NEWLIB
 
 # Needed by us
 MOD_USE=
