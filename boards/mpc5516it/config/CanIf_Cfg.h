69e1a86283b9eb679c4514f17412dfff6dcad249
eija <devnull@localhost>
2010-12-20 15:52:03 +0100

    Removing CanIf_CallbackType in CanIf_Cfg.h-files.

--- boards/mpc5516it/config/CanIf_Cfg.h
+++ boards/mpc5516it/config/CanIf_Cfg.h
@@ -91,17 +91,6 @@ typedef enum {
 #define CANIF_TRANSCEIVER_API               STD_OFF  /**< Not supported */
 #define CANIF_TRANSMIT_CANCELLATION         STD_OFF  /**< Not supported */
 
-//-------------------------------------------------------------------
-
-typedef struct {
-	void (*CancelTxConfirmation)( void *);  // (const Can_PduType *);
-	void (*RxIndication)(void *); //(const Can_PduType *);
-	void (*ControllerBusOff)(uint8);
-	void (*ControllerWakeup)(uint8);
-	void (*Arc_Error)(uint8,uint32);
-} CanIf_CallbackType;
-
-
 
 //-------------------------------------------------------------------
 
