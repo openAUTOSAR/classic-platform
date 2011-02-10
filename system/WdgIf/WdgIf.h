1129c634654dd490d1a4a5ce51275fef8717b8c0
Fredrik <devnull@localhost>
2011-02-10 19:19:38 +0100

    Added Det reporting to WdgIf.

--- system/WdgIf/WdgIf.h
+++ system/WdgIf/WdgIf.h
@@ -11,6 +11,11 @@
 #include "Std_Types.h"
 #include "WdgIf_Cfg.h"
 
+#define WDGIF_SETMODE_ID             0x01
+#define WDGIF_TRIGGER_ID             0x02
+
+#define WDGIF_E_PARAM_DEVICE         0x01
+
 Std_ReturnType WdgIf_SetMode (uint8 DeviceIndex, WdgIf_ModeType Mode);
 
 void WdgIf_Trigger (uint8 DeviceIndex);
