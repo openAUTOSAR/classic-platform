b9fd1615a318bc75bfd81c81d7fd614cea24358b
maek <devnull@localhost>
2011-06-30 17:31:42 +0200

    Made rte_simple example more advanced by adding a blinker.

--- examples/rte_simple/rte_simple.c
+++ examples/rte_simple/rte_simple.c
@@ -15,6 +15,9 @@
 
 #include "Com.h"
 #include "Os.h"
+#include "Dio.h"
+#include "EcuM.h"
+#include "CanIf.h"
 
 /*
  * Functions that must be supplied by the example
@@ -23,8 +26,35 @@ void OsIdle( void ) {
 	for(;;);
 }
 
+void StartupTask( void ) {
+
+	// Call second phase of startup sequence.
+	EcuM_StartupTwo();
+
+	// Startup CanIf due to ComM is missing in this example
+	CanIf_InitController(CANIF_Channel_1, CANIF_Channel_1_CONFIG_0);
+	CanIf_SetControllerMode(CANIF_Channel_1, CANIF_CS_STARTED);
+
+	// Make sure that the right PDU-groups are ready for communication.
+	Com_IpduGroupStart(ComPduGroup, 0);
+
+	// End of startup_task().
+	TerminateTask();
+
+}
+
 void MainFunctionTask( void ) {
 	Com_MainFunctionRx();
 	Com_MainFunctionTx();
+	TerminateTask();
+}
+
+
+
+// Task that toggles the LED
+Dio_LevelType level = 1;
+void BlinkerTask( void ) {
+	level = !level;
+	Dio_WriteChannel(DIO_CHANNEL_NAME_LED_CHANNEL, level);
 	TerminateTask();
 }
