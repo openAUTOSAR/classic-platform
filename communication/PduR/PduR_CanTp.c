cf16857e3c6a94a428946b05c99ff0d240ba5b5f
eija <devnull@localhost>
2011-08-12 14:28:56 +0200

    lint fixes in PduR. TODO in PduR_CanTp.c.

--- communication/PduR/PduR_CanTp.c
+++ communication/PduR/PduR_CanTp.c
@@ -28,10 +28,10 @@ BufReq_ReturnType PduR_CanTpProvideRxBuffer(PduIdType CanTpRxPduId, PduLengthTyp
 void PduR_CanTpRxIndication(PduIdType CanTpRxPduId, NotifResultType Result) {
 	PduInfoType PduInfo = {
 		.SduDataPtr = &Result
+		//TODO: .SduLength = ?? // To fix PC-Lint 785 add length of SDU
 	};
 	PduR_ARC_RxIndication(CanTpRxPduId, &PduInfo, 0x04);
 }
-
 
 BufReq_ReturnType PduR_CanTpProvideTxBuffer(PduIdType CanTpTxPduId, PduInfoType** PduInfoPtr, uint16 Length) {
 	return PduR_ARC_ProvideTxBuffer(CanTpTxPduId, PduInfoPtr, Length, 0x03);
