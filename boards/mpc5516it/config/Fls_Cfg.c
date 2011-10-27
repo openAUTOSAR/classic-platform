6871c4d237a11bcd2fbc775e0e7429d9abc7df90
jcar <devnull@localhost>
2011-10-27 11:35:28 +0200

    Fixed Fls for mpc5567 and 5606s

--- boards/mpc5516it/config/Fls_Cfg.c
+++ boards/mpc5516it/config/Fls_Cfg.c
@@ -137,53 +137,112 @@ const FlashType flashInfo[] = {
 	 *
 	 * LOW   8*16KB + 2*64KB   = 256KB
 	 * MID   2*128KB           = 256KB
-	 * HIGH  2*256KB           = 1.5MB
-	 *   	 2*256KB
-	 *       2*256KB
+	 * HIGH  4*128KB           = 512KB
 	 */
 
 const FlashType flashInfo[] = {
-	/* LOW 0-9 */
 	[0].sectCnt = 16,
 	[0].bankSize = 0x100000,
 	[0].regBase = 0xffff8000UL,
-	[0].sectAddr[0] = 0, 	     /* 0, B0F0, LOW  */
+
+	/* LOW */
+	[0].sectAddr[0] = 0,
 	[0].addrSpace[0] = ADDR_SPACE(0, ADDR_SPACE_LOW, 1 ),
-	[0].sectAddr[1] = 0x00004000, /* 1, B0F1, LOW */
+	[0].sectAddr[1] = 0x00004000,
 	[0].addrSpace[1] = ADDR_SPACE(1, ADDR_SPACE_LOW, 1 ),
-	[0].sectAddr[2] = 0x00008000, /* 2, B0F2, LOW */
+	[0].sectAddr[2] = 0x00008000,
 	[0].addrSpace[2] = ADDR_SPACE(2, ADDR_SPACE_LOW, 1 ),
-	[0].sectAddr[3] = 0x0000c000, /* 3, B0F3, LOW */
+	[0].sectAddr[3] = 0x0000c000,
 	[0].addrSpace[3] = ADDR_SPACE(3, ADDR_SPACE_LOW, 1 ),
-	[0].sectAddr[4] = 0x00010000, /* 4, B0F4, LOW */
+	[0].sectAddr[4] = 0x00010000,
 	[0].addrSpace[4] = ADDR_SPACE(4, ADDR_SPACE_LOW, 2 ),
-	[0].sectAddr[5] = 0x00014000, /* 5, B0F5, LOW */
+	[0].sectAddr[5] = 0x00014000,
 	[0].addrSpace[5] = ADDR_SPACE(5, ADDR_SPACE_LOW, 2 ),
-	[0].sectAddr[6] = 0x00018000, /* 6, B0F6, LOW */
+	[0].sectAddr[6] = 0x00018000,
 	[0].addrSpace[6] = ADDR_SPACE(6, ADDR_SPACE_LOW, 2 ),
-	[0].sectAddr[7] = 0x0001c000, /* 7, B0F7, LOW */
+	[0].sectAddr[7] = 0x0001c000,
 	[0].addrSpace[7] = ADDR_SPACE(7, ADDR_SPACE_LOW, 2 ),
-	[0].sectAddr[8] = 0x00020000, /* 8, B0F8, LOW */
+	[0].sectAddr[8] = 0x00020000,
 	[0].addrSpace[8] = ADDR_SPACE(8, ADDR_SPACE_LOW, 3 ),
-	[0].sectAddr[9] = 0x00030000, /* 9, B0F9, LOW */
+	[0].sectAddr[9] = 0x00030000,
 	[0].addrSpace[9] = ADDR_SPACE(9, ADDR_SPACE_LOW, 3 ),
 
 	/* MID */
-	[0].sectAddr[10] = 0x00040000, /* 0, B0F10, MID */
+	[0].sectAddr[10] = 0x00040000,
 	[0].addrSpace[10] = ADDR_SPACE(0, ADDR_SPACE_MID, 4 ),
-	[0].sectAddr[11] = 0x00060000, /* 1, B0F11, MID */
+	[0].sectAddr[11] = 0x00060000,
 	[0].addrSpace[11] = ADDR_SPACE(1, ADDR_SPACE_MID, 4 ),
 
 	/* HIGH */
-	[0].sectAddr[12] = 0x00080000, /* 0, B0F12, HIGH */
+	[0].sectAddr[12] = 0x00080000,
 	[0].addrSpace[12] = ADDR_SPACE(0, ADDR_SPACE_HIGH, 5 ),
-	[0].sectAddr[13] = 0x000a0000, /* 1, B0F13, HIGH */
+	[0].sectAddr[13] = 0x000a0000,
 	[0].addrSpace[13] = ADDR_SPACE(1, ADDR_SPACE_HIGH, 5 ),
-	[0].sectAddr[14] = 0x000c0000, /* 2, B0F14, HIGH */
+	[0].sectAddr[14] = 0x000c0000,
 	[0].addrSpace[14] = ADDR_SPACE(2, ADDR_SPACE_HIGH, 6 ),
-	[0].sectAddr[15] = 0x000e0000, /* 3, B0F15, HIGH */
+	[0].sectAddr[15] = 0x000e0000,
 	[0].addrSpace[15] = ADDR_SPACE(3, ADDR_SPACE_HIGH, 6 ),
 	[0].sectAddr[16] = 0x00100000,	/* End, NOT a sector */
+};
+#elif defined(CFG_MPC5567)
+/* RWW between partitions*
+ *
+ * LOW   2*16KB + 2*48KB + 2*64KB = 256KB
+ * MID   2*128KB                  = 256KB
+ * HIGH  12*128KB                 = 1.5MB
+ */
+
+const FlashType flashInfo[] = {
+[0].sectCnt = 20,
+[0].bankSize = 0x200000,
+[0].regBase = 0xc3f88000UL,
+
+/* LOW  */
+[0].sectAddr[0] = 0,
+[0].addrSpace[0] = ADDR_SPACE(0, ADDR_SPACE_LOW, 1 ),
+[0].sectAddr[1] = 0x00004000,
+[0].addrSpace[1] = ADDR_SPACE(1, ADDR_SPACE_LOW, 1 ),
+[0].sectAddr[2] = 0x00010000,
+[0].addrSpace[2] = ADDR_SPACE(2, ADDR_SPACE_LOW, 1 ),
+[0].sectAddr[3] = 0x00018000,
+[0].addrSpace[3] = ADDR_SPACE(3, ADDR_SPACE_LOW, 1 ),
+[0].sectAddr[4] = 0x00020000,
+[0].addrSpace[4] = ADDR_SPACE(4, ADDR_SPACE_LOW, 2 ),
+[0].sectAddr[5] = 0x00030000,
+[0].addrSpace[5] = ADDR_SPACE(5, ADDR_SPACE_LOW, 2 ),
+
+/* MID */
+[0].sectAddr[6] = 0x00040000,
+[0].addrSpace[6] = ADDR_SPACE(0, ADDR_SPACE_MID, 3 ),
+[0].sectAddr[7] = 0x00060000,
+[0].addrSpace[7] = ADDR_SPACE(1, ADDR_SPACE_MID, 3 ),
+
+/* HIGH */
+[0].sectAddr[8] = 0x00080000,
+[0].addrSpace[8] = ADDR_SPACE(0, ADDR_SPACE_HIGH, 4 ),
+[0].sectAddr[9] = 0x000a0000,
+[0].addrSpace[9] = ADDR_SPACE(1, ADDR_SPACE_HIGH, 4 ),
+[0].sectAddr[10] = 0x000c0000,
+[0].addrSpace[10] = ADDR_SPACE(2, ADDR_SPACE_HIGH, 5 ),
+[0].sectAddr[11] = 0x000e0000,
+[0].addrSpace[11] = ADDR_SPACE(3, ADDR_SPACE_HIGH, 5 ),
+[0].sectAddr[12] = 0x00100000,
+[0].addrSpace[12] = ADDR_SPACE(0, ADDR_SPACE_HIGH, 6 ),
+[0].sectAddr[13] = 0x00120000,
+[0].addrSpace[13] = ADDR_SPACE(1, ADDR_SPACE_HIGH, 6 ),
+[0].sectAddr[14] = 0x00140000,
+[0].addrSpace[14] = ADDR_SPACE(2, ADDR_SPACE_HIGH, 7 ),
+[0].sectAddr[15] = 0x00160000,
+[0].addrSpace[15] = ADDR_SPACE(3, ADDR_SPACE_HIGH, 7 ),
+[0].sectAddr[16] = 0x00180000,
+[0].addrSpace[16] = ADDR_SPACE(0, ADDR_SPACE_HIGH, 8 ),
+[0].sectAddr[17] = 0x001a0000,
+[0].addrSpace[17] = ADDR_SPACE(1, ADDR_SPACE_HIGH, 8 ),
+[0].sectAddr[18] = 0x001c0000,
+[0].addrSpace[18] = ADDR_SPACE(2, ADDR_SPACE_HIGH, 9 ),
+[0].sectAddr[19] = 0x001e0000,
+[0].addrSpace[19] = ADDR_SPACE(3, ADDR_SPACE_HIGH, 9 ),
+[0].sectAddr[20] = 0x00200000,	/* End, NOT a sector */
 };
 #else
 #error CPU NOT supported
