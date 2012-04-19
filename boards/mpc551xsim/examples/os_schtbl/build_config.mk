e062a0a22d46ffa5c1282aa07e2c16d4a3733823
jcar <devnull@localhost>
2012-04-19 13:19:12 +0200

    Generated and built in-tree examples with 551xsim, not tested

--- boards/mpc551xsim/examples/os_schtbl/build_config.mk
+++ boards/mpc551xsim/examples/os_schtbl/build_config.mk
@@ -1,3 +1,15 @@
 
+# Version of build system
+REQUIRED_BUILD_SYSTEM_VERSION=1.0.0
 
-MOD_USE+= DET ECUM MCU KERNEL RAMLOG
+# Get configuration makefiles
+-include ../config/*.mk
+-include ../config/$(BOARDDIR)/*.mk
+
+
+# Project settings
+
+SELECT_CONSOLE = RAMLOG
+
+SELECT_OPT = OPT_DEBUG 
+
