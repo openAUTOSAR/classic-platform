5e25c8c2ac5ed97e6ddf546eea70e33b31de3cd0
joek <devnull@localhost>
2012-06-24 18:47:47 +0200

    Fix deadline monitoring for defered signals in Ticket #1460

--- communication/Com/Com_misc.c
+++ communication/Com/Com_misc.c
@@ -152,13 +152,20 @@ void Com_WriteSignalDataToPdu(
 	// Get PDU
 	const ComSignal_type *Signal     = GET_Signal(signalId);
 	const ComIPdu_type   *IPdu       = GET_IPdu(Signal->ComIPduHandleId);
+	const void* pduDataPtr = 0;
+
+	if (IPdu->ComIPduSignalProcessing == DEFERRED && IPdu->ComIPduDirection == RECEIVE) {
+		pduDataPtr = IPdu->ComIPduDeferredDataPtr;
+	} else {
+		pduDataPtr = IPdu->ComIPduDataPtr;
+	}
 
 	// Get data
 	Com_WriteSignalDataToPduBuffer(
 			signalId,
 			FALSE,
 			signalData,
-			(void *)IPdu->ComIPduDataPtr,
+			pduDataPtr,
 			IPdu->ComIPduSize);
 }
 
