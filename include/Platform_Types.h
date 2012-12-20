88f3b15cf1af4ba1b4bd53e7c4b6f16c9b00dcd9
mahi <devnull@localhost>
2012-12-20 10:53:47 +0100

    Added timer for Arm Cortex_M3. Hopefully fiex msys path conversion.

--- include/Platform_Types.h
+++ include/Platform_Types.h
@@ -55,5 +55,18 @@ typedef signed long         sint32_least;
 typedef float               float32; 
 typedef double              float64;  
 
+
+typedef volatile int8_t vint8_t;
+typedef volatile uint8_t vuint8_t;
+
+typedef volatile int16_t vint16_t;
+typedef volatile uint16_t vuint16_t;
+
+typedef volatile int32_t vint32_t;
+typedef volatile uint32_t vuint32_t;
+
+typedef volatile int64_t vint64_t;
+typedef volatile uint64_t vuint64_t;
+
 #endif
 /** @} */
