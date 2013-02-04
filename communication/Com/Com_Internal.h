af38b6b754bf6a81969ac8a6ce24be40230c99cc
jcar <devnull@localhost>
2013-02-04 11:37:49 +0100

    Fixed so that Com builds with CodeWarrior when function have no return values ticket #1911

--- communication/Com/Com_Internal.h
+++ communication/Com/Com_Internal.h
@@ -48,18 +48,18 @@ extern Com_Arc_Config_type Com_Arc_Config;
 	if (PduId >= COM_N_IPDUS) { \
 		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_PDU_ID); \
 		return __VA_ARGS__; \
-	} \
+	}
 
 #define PDU_ID_CHECK_NO_RETURN(PduId,ApiId) \
 	if (PduId >= COM_N_IPDUS) { \
 		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_PDU_ID); \
-	} \
+	}
 
 #define VALIDATE_SIGNAL(SignalId, ApiId, ...) \
 	if (SignalId >= COM_N_SIGNALS) { \
 		DET_REPORTERROR(COM_MODULE_ID, COM_INSTANCE_ID, ApiId, COM_INVALID_SIGNAL_ID); \
 		return __VA_ARGS__; \
-	} \
+	}
 
 
 #else
