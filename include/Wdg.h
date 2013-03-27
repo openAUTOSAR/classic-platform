31010b5ead2245381eb61118c1d16fc4e1c9b2e7
mahi <devnull@localhost>
2013-03-27 08:00:23 +0100

    More header corrections

--- include/Wdg.h
+++ include/Wdg.h
@@ -11,8 +11,7 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
- * -------------------------------- Arctic Core ------------------------------
- */
+ * -------------------------------- Arctic Core ------------------------------*/
 
 #ifndef WDG_H_
 #define WDG_H_
