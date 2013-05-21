b3915840566e2ad7911d4c107a5158be6dc6c793
mahi <devnull@localhost>
2013-05-21 15:05:03 +0200

    Fixed ticket #2315

--- memory/NvM/NvM.c
+++ memory/NvM/NvM.c
@@ -972,7 +972,7 @@ static void DriveBlock( const NvM_BlockDescriptorType	*bPtr,
 						case MEMIF_BLOCK_INCONSISTENT:
 							/* @req 3.1.5/NVM360 but is overridden by NVM172 (implicit revovery) */
 							admPtr->ErrorStatus = NVM_REQ_INTEGRITY_FAILED;
-							DEM_REPORTERRORSTATUS(NVM_E_REQ_INTEGRITY_FAILED,DEM_EVENT_STATUS_FAILED);
+							DEM_REPORTERRORSTATUS(NVM_E_INTEGRITY_FAILED,DEM_EVENT_STATUS_FAILED);
 							break;
 						case MEMIF_JOB_FAILED:
 							/* @req 3.1.5/NVM361 */
@@ -1454,7 +1454,7 @@ Std_ReturnType NvM_InvalidateNvBlock( NvM_BlockIdType blockId  )
     if( admPtr->BlockWriteProtected || admPtr->DataIndex >= bPtr->NvBlockNum) {
         /* If write protected or ROM block in dataset, return E_NOT_OK */
 #if NVM_AR_VERSION >= 40300
-        DEM_REPORTERRORSTATUS(NVM_E_REQ_INTEGRITY_FAILED, DEM_EVENT_STATUS_FAILED);
+        DEM_REPORTERRORSTATUS(NVM_E_INTEGRITY_FAILED, DEM_EVENT_STATUS_FAILED);
 #endif
         /* @req 4.0.3|3.1.5/NVM027 */
         DET_REPORTERROR(MODULE_ID_NVM, NVM_INVALIDATENV_BLOCK_ID,
