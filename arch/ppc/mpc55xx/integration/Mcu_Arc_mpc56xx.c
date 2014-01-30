19f020bfe32aa5bb7b488df669d6963b5289e589
mahi <devnull@localhost>
2014-01-30 10:02:53 +0100

    Added sleep for mpc560x

--- arch/ppc/mpc55xx/integration/Mcu_Arc_mpc56xx.c
+++ arch/ppc/mpc55xx/integration/Mcu_Arc_mpc56xx.c
@@ -28,8 +28,13 @@
 #endif
 #include "asm_ppc.h"
 #include "Os.h"
+#include "EcuM.h"
 
 /* ----------------------------[private define]------------------------------*/
+
+#define MODE_RESET		0
+#define MODE_STANDBY 	0xd
+
 /* ----------------------------[private macro]-------------------------------*/
 /* ----------------------------[private typedef]-----------------------------*/
 /* ----------------------------[private function prototypes]-----------------*/
@@ -41,6 +46,61 @@ extern uint32 EccErrReg;
 
 
 /* ----------------------------[private functions]---------------------------*/
+
+static void enterStandby( void ) {
+
+	/* ME_PCTL_x MUST be off */
+
+
+	/* Configure STANDBY0 Mode for lowest consumption (only WUP Unit ON) */
+	/* Please note, WKPU (Wakeup Unit) is always enabled */
+	/* To generate an interrupt event triggered by a wakeup line, it is */
+	/* necessary to enable WKPU */
+
+	RGM.STDBY.B.BOOT_FROM_BKP_RAM = 1;
+	ME.MER.B.STANDBY0 = 1;
+	PCU.PCONF[2].B.STBY0 = 1;		/* Enable 32K RAM in stdby */
+
+	/* Turn off ALL ME_PCTL
+	 * All ME_PCTL point to LP config ME_LP_PC0 (LP_CFG=0)
+	 */
+	ME.LPPC[0].R = 0;
+	ME.RUNPC[0].R = 0xfe;
+	ME.RUNPC[1].R = 0xfe;
+
+
+	/* Errata e3247, must write FRZ to all CAN devices. How do I know
+	 * what ones to disable?
+	 * */
+	CAN_0.MCR.B.FRZ = 1;
+	CAN_1.MCR.B.FRZ = 1;
+	CAN_2.MCR.B.FRZ = 1;
+	CAN_3.MCR.B.FRZ = 1;
+	CAN_4.MCR.B.FRZ = 1;
+	CAN_5.MCR.B.FRZ = 1;
+
+	ME.STANDBY0.B.PDO=1;	  /* Keep output state of the Pad Power Driver */
+	ME.STANDBY0.B.MVRON=0;	  /* Main Voltage regulator is off */
+	ME.STANDBY0.B.DFLAON=1;   /* Data flash is in power-down mode */
+	ME.STANDBY0.B.CFLAON=1;   /* Code flash is in power-down mode */
+	ME.STANDBY0.B.FXOSC0ON=0;    /* Fast external crystal oscillator (4-16 MHz) off */
+	ME.STANDBY0.B.FMPLLON=0;    /* FMPLL is off (default status during STOP0) */
+	ME.STANDBY0.B.SYSCLK=0xF; /* System clock disabled BEFORE RC switching off */
+	ME.STANDBY0.B.FIRCON=0;     /* Fast internal RC oscillator (16 MHz) off */
+
+
+	/* Enter STANDBY Mode */
+	ME.MCTL.R = ((vuint32_t)MODE_STANDBY<<28) | 0x00005AF0; /* Mode & Key */
+	ME.MCTL.R = ((vuint32_t)MODE_STANDBY<<28) | 0x0000A50F; /* Mode & Key */
+
+	/* Wait to enter sleep and come back in either flash or RAM */
+	 while(1 == ME.GS.B.S_MTRANS)
+	    {
+	    }
+
+	/* From "MC_ME Mode Diagram", page 136 in RM,  it seems as e return to DRUN after exit of STANDBY0 */
+}
+
 /* ----------------------------[public functions]----------------------------*/
 
 
@@ -67,6 +127,7 @@ void Mcu_Arc_InitClockPre( const Mcu_ClockSettingConfigType *clockSettingsPtr )
     ME.RUNPC[1].R = 0x00000010;
     /* MPC56xxB/S: select ME.RUNPC[1] */
     ME.PCTL[68].R = 0x01; //SIUL control
+    ME.PCTL[69].R = 0x01; //WKUP control
     ME.PCTL[91].R = 0x01; //RTC/API control
     ME.PCTL[92].R = 0x01; //PIT_RTI control
     ME.PCTL[72].R = 0x01; //eMIOS0 control
@@ -82,6 +143,7 @@ void Mcu_Arc_InitClockPre( const Mcu_ClockSettingConfigType *clockSettingsPtr )
     ME.PCTL[23].R = 0x01; //DMAMUX control
     ME.PCTL[48].R = 0x01; /* MPC56xxB/P/S LINFlex  */
     ME.PCTL[49].R = 0x01; /* MPC56xxB/P/S LINFlex  */
+
     /* Mode Transition to enter RUN0 mode: */
     /* Enter RUN0 Mode & Key */
     ME.MCTL.R = 0x40005AF0;
@@ -196,15 +258,97 @@ void Mcu_Arc_InitClockPost( const Mcu_ClockSettingConfigType *clockSettingsPtr )
 }
 
 
+extern char __LP_TEXT_ROM[];
+extern char __LP_TEXT_START[];
+extern char __LP_TEXT_END[];
+
+/* Context save area */
+uint8_t context[32+5];
+
+/**
+ *
+ * @param mcuMode The mcuMode from Mcu_SetMode()
+ */
 void Mcu_Arc_SetModePre( Mcu_ModeType mcuMode)
 {
-	(void)mcuMode;
+	uint32_t timeout = 0;
+#if defined(CFG_ECUM_VIRTUAL_SOURCES)
+	EcuM_WakeupSourceType pendWakeup;
+#endif
+
+	if( mcuMode == MCU_MODE_NORMAL ) {
+		mcuMode = MCU_MODE_RUN;
+	}
+
+	if( MCU_MODE_RUN == mcuMode ) {
+
+	} else if( MCU_MODE_SLEEP == mcuMode  ) {
+
+		memcpy( __LP_TEXT_START, __LP_TEXT_ROM, __LP_TEXT_END - __LP_TEXT_START );
+
+
+#if defined(CFG_ECUM_VIRTUAL_SOURCES)
+
+		do {
+			/* Go to sleep */
+			if( Mcu_Arc_setjmp(context) == 0 ) {
+				enterStandby();
+			}
+
+			/* Back from sleep!
+			 * Now running in DRUN and on FIRC (16Mhz)
+			 */
+
+#if defined(USE_ECUM)
+		EcuM_CheckWakeup( 0x3fffffffUL );
+#endif
+
+			pendWakeup = EcuM_GetPendingWakeupEvents();
+			if( pendWakeup & (ECUM_WKSOURCE_WAKEUPSOURCEPIN | ECUM_WKSOURCE_WAKEUPSOURCEVIRTUALPIN) ) {
+				break;
+			}
+		} while( pendWakeup & ECUM_WKSOURCE_WAKEUPSOURCERTC );
+#else
+		/* Go to sleep */
+		if( Mcu_Arc_setjmp(context) == 0 ) {
+			enterStandby();
+		}
+
+		/* Back from sleep!
+		 * Now running in DRUN and on FIRC (16Mhz)
+		 */
+
+#if defined(USE_ECUM)
+	EcuM_CheckWakeup( 0x3fffffffUL );
+#endif
+#endif
+
+		/* Wdg OFF */
+		SWT.SR.R = 0x0000c520;     /* Write keys to clear soft lock bit */
+		SWT.SR.R = 0x0000d928;
+		SWT.CR.R = 0x8000010A;
+
+		/* Setup exceptions and INTC again */
+		Os_IsrInit();
+
+
+	}	/* MCU_MODE_SLEEP == mcuMode */
+
+
+#if defined(CFG_MCU_ARC_CONFIG)
+	Mcu_Arc_SetModePre2(mcuMode, Mcu_Arc_ConfigData.sleepConfig );
+#endif
 }
 
+/**
+ *
+ * @param mcuMode The mcuMode from Mcu_SetMode()
+ */
 void Mcu_Arc_SetModePost( Mcu_ModeType mcuMode)
 {
-	(void)mcuMode;
+#if defined(CFG_MCU_ARC_CONFIG)
+	Mcu_Arc_SetModePost2(mcuMode,  Mcu_Arc_ConfigData.sleepConfig);
+#endif
 }
-
 
 
